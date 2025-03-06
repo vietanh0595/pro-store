8df0987922d26386b785f306c96e42d00dab761a
VA <duongvietanh0495@gmail.com>
2025-03-06 20:29:36 +0700

    initialize project and basic layout

---
+++ app/(root)/page.tsx
@@ -0,0 +1,16 @@
+import ProductList from "@/components/shared/header/product/product-list";
+import sampleData from "@/db/sample-data";
+
+const HomePage = () => {
+  return (
+    <div className="space-y-8">
+      <h2 className="h2-bold">Latest Products</h2>
+      <ProductList
+        title="Newest Arrivals"
+        data={sampleData.products}
+        limit={4}
+      />
+    </div>
+  );
+};
+export default HomePage;
